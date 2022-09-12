c9594be91735078ae2ea22c4d4300489593e6242
Karen Baltazar <a01707750@tec.mx>
2022-09-12 14:47:52 -0500

    analytics comp - finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Analytics from "./components/Analytics";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from './components/Navbar'
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
       <Footer />
     </div>
   );
