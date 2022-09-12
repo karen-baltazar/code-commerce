8811085c7c984fcdd03b59015b53fd335764ea3f
Karen Baltazar <a01707750@tec.mx>
2022-09-12 15:19:22 -0500

    newsletter comp - finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Analytics from "./components/Analytics";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from './components/Navbar'
+import Newsletter from "./components/Newsletter";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
       <Footer />
     </div>
   );
