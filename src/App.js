9b227b0e71a4c5abd5fb76a372be8e4302f4514a
Karen Baltazar <a01707750@tec.mx>
2022-09-12 14:14:16 -0500

    hero definition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
       <Footer />
     </div>
   );
