a36d410cbd10547ca333ea1f3efc1d5834d163cd
Karen Baltazar <a01707750@tec.mx>
2022-09-12 15:55:08 -0500

    footer comp - finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
         <div>
             <h1 className='w-full text-3xl font-bold text-[#00df8a]'>REACT.</h1>
             <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget mauris metus. Phasellus tempus nulla non massa efficitur, a convallis erat dictum.</p>
-            <div>
+            <div className='flex justify-between md:w-[75%] my-6'>
                 <FaFacebookSquare size={30}/>
                 <FaInstagramSquare size={30}/>
                 <FaTwitterSquare size={30}/>
@@ -21,7 +21,45 @@ const Footer = () => {
                 <FaDribbbleSquare size={30}/>
             </div>
         </div>
-        
+        <div className='lg:cols-span-2 flex justify-between mt-6'>
+          <div>
+            <h6 className='font-medium text-gray-400'>Solutions</h6>
+            <ul>
+              <li className='py-2 text-sm'>Analytics</li>
+              <li className='py-2 text-sm'>Marketing</li>
+              <li className='py-2 text-sm'>Commerce</li>
+              <li className='py-2 text-sm'>Insights</li>
+            </ul>
+          </div>
+          <div>
+            <h6 className='font-medium text-gray-400'>Support</h6>
+            <ul>
+              <li className='py-2 text-sm'>Pricing</li>
+              <li className='py-2 text-sm'>Documentation</li>
+              <li className='py-2 text-sm'>Guides</li>
+              <li className='py-2 text-sm'>API Status</li>
+            </ul>
+          </div>
+          <div>
+            <h6 className='font-medium text-gray-400'>Company</h6>
+            <ul>
+              <li className='py-2 text-sm'>About</li>
+              <li className='py-2 text-sm'>Blog</li>
+              <li className='py-2 text-sm'>Jobs</li>
+              <li className='py-2 text-sm'>Press</li>
+              <li className='py-2 text-sm'>Careers</li>
+            </ul>
+          </div>
+          <div>
+            <h6 className='font-medium text-gray-400'>Legal</h6>
+            <ul>
+              <li className='py-2 text-sm'>Claims</li>
+              <li className='py-2 text-sm'>Policy</li>
+              <li className='py-2 text-sm'>Jobs</li>
+              <li className='py-2 text-sm'>Terms</li>
+            </ul>
+          </div>
+        </div>
     </div>
   )
 }
